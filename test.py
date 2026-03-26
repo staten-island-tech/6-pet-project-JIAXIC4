c16f8121fe7030868e39986d071f6fef06713876
JIAXIC4 <JIAXIC4@NYCSTUDENTS.NET>
2026-03-26 10:17:02 -0400

    finished test

--- test.py
+++ test.py
@@ -1,28 +1,25 @@
 def tree_height(N, h):
-    highest_streak = 0 
-    lowest_streak = 0
-    streak_high = 0 
-    streak_low = 0 
-    for i in range(h):
-        if i in range(h):
-            if h[i] > h[i] + 1:
-                streak_high+= 1 
-                h[i] = h[i] + 1
-                if streak_high > highest_streak:
-                    highest_streak = streak_high
+    highest_streak = 1 
+    lowest_streak = 1
+    streak_high = 1 
+    streak_low = 1 
+    for i in range(len(h) - 1):
+        if h[i] < h[i + 1]:
+            streak_high+= 1 
+            if streak_high > highest_streak:
+                highest_streak = streak_high
 
-            if h[i] < h[i] + 1:
-                streak_high = 0
-                h[i] = h[i] + 1
+        elif h[i] > h[i + 1]:
+            streak_high = 1
 
-            elif h[i] > h[i] + 1:
-                streak_low += 1
-                h[i] = h[i] +1
-                if streak_low > lowest_streak:
-                    lowest_streak = streak_low
+        if h[i] > h[i + 1]:
+            streak_low += 1
+            if streak_low > lowest_streak:
+                lowest_streak = streak_low
 
-            if h[i] > h[i] + 1:
-                streak_high = 0
-                h[i] = h[i] + 1
+        elif h[i] < h[i + 1]:
+            streak_low = 1
 
-tree_height(5, [3, 2, 5, 4, 1])
+    print(highest_streak, lowest_streak)
+tree_height(10, [2, 1 ,4 , 6, 8 , 2, 9, 5, 2, 3])
+tree_height(4, [1, 3, 4, 2])
